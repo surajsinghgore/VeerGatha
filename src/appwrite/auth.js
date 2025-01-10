d4d304edc8b0daa9f57597114bdd3266363c98e8
suraj singh rajput <surajthakurrs45@gmail.com>
2025-01-10 14:10:33 +0530

    appwrite auth service ready

---
+++ src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwrite_endpoint).setProject(conf.appwrite_project_id);
+    this.account = new Account(this.client);
+  }
+
+  // create account
+  async createAccount({ email, password, name }) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        // call another method to make them login directly
+        this.loginUser({ email, password });
+        return userAccount;
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //   login user
+  async loginUser({ email, password }) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // getCurrentUser data
+  async getCurrentUser() {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("current user is not logged in");
+      throw error;
+    }
+  }
+
+  //   logout user
+  async logoutCurrentUser() {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("current user is not logged in");
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,10 +1,10 @@
 
 
 const conf={
-   appwrite_url: String(import.meta.env.VITE_APPWRITE_URL),
-    appwrite_project_id: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwrite_database_id: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwrite_collection_id: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+   appwriteEndPoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwrite_bucket_id: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
    
 }
